6fa81b462b1c2b4bef80a0a7505182a75786358e
gvloginov <gennadiy.loginov@rt.ru>
2021-08-10 20:06:47 +0300

    Lesson 9: Typescript (часть 2)

---
+++ src/lesson9/easy1.ts
@@ -0,0 +1,25 @@
+// Задание первого уровня 1
+// В функцию приходит массив состояний заказа и фильтруется
+// Нужно заменить FIXME на тип который вычисляется на освове OrderState
+
+type FIXME = any;
+
+const orderStates = [
+  "initial",
+  "inWork",
+  "buyingSupplies",
+  "producing",
+  "fullfilled",
+] as const;
+
+type OrderState = typeof orderStates[number];
+
+export const getUserOrderStates = (orderStates: OrderState[]): OrderState[] => {
+  const filteredStates = [] as OrderState[];
+  orderStates.forEach((element) => {
+    if (element !== "buyingSupplies" && element !== "producing") {
+      filteredStates.push(element);
+    }
+  });
+  return filteredStates;
+};
